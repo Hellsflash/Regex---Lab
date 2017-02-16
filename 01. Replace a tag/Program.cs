82c4a31b4725b45f6bb045434253a18e5a7afddd
Mario <Mario@MARIO-PC>
2017-02-16 13:49:47 +0200

    Add project files.

---
+++ 01. Replace a tag/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01.Replace_a_tag
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            while (input!="end")
+            {
+                var pattern = @"<a.?href.*?=(.*)>(.*?)<\/a>";
+
+                var replace = @"[URL href=$1]$2[/URL]";
+
+                var repalced = Regex.Replace(input, pattern, replace);
+
+                Console.WriteLine(repalced);
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
